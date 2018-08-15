b732e084c4afa77606f31b7a289c0f6f7afd9c51
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2018-08-15 18:23:46 +0200

    a rename

--- lib/MininkSend.hs
+++ lib/MininkSend.hs
@@ -10,14 +10,14 @@ import Data.DateTime (fromSeconds, toGregorian')
 
 import Control.Monad (void)
 
-isOnSameDay :: Integer -> Integer -> Bool
-isOnSameDay t1 t2 = (toDayPrecision t1) == (toDayPrecision t2)
+areOnSameDay :: Integer -> Integer -> Bool
+areOnSameDay t1 t2 = (toDayPrecision t1) == (toDayPrecision t2)
   where toDayPrecision :: Integer -> (Integer, Int, Int)
         toDayPrecision = toGregorian'.fromSeconds
 
 send :: EmailSender m => Integer -> Subscription -> m ()
 send currentTime (Subscription _ lastSent subscriber) =
-  if isOnSameDay currentTime lastSent then return () else void $ sendEmail subscriber ""
+  if areOnSameDay currentTime lastSent then return () else void $ sendEmail subscriber ""
 
 sendDailyMails :: (Epoch m, EmailSender m, SubscriptionDb m) => m (Either String ())
 sendDailyMails = do
