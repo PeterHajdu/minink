cef4349886796c93f2c1a684604ed4e97b822ec4
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2018-08-16 15:44:53 +0200

    rewrite sender to use the lib

--- src-send/Main.hs
+++ src-send/Main.hs
@@ -1,50 +1,106 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Subscription (Subscription(..))
-import Control.Applicative
-import Database.SQLite.Simple
-import Database.SQLite.Simple.FromRow
-import Data.DateTime (fromSeconds, toGregorian', getCurrentTime, toSeconds)
+import MininkSend
+import LessonDb
+import Time
+import EmailSender
+import Subscription
+import SubscriptionDb
+import Control.Monad.Reader (ReaderT, runReaderT)
+import Control.Monad.Reader (MonadReader, asks)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Data.Text.Lazy as LT
 
 import Network.HaskellNet.SMTP
 import Network.HaskellNet.Auth
 import Network.HaskellNet.SMTP.SSL
-import qualified Data.Text.Lazy as T
+import Control.Exception (handle, SomeException)
+import Data.DateTime (getCurrentTime, toSeconds)
+
+import qualified Database.SQLite.Simple as SQL
+import Database.SQLite.Simple (NamedParam(..))
 import qualified Data.Text.IO as TIO
-import System.Environment (lookupEnv)
 
-sendEmail :: String -> SMTPConnection -> Integer -> Connection -> Subscription -> IO ()
-sendEmail mininkBase smtpConn today conn subs = do
-  print subs
-  let lessonBase = mininkBase ++ "lessons/"
-  let filename = lessonBase ++ (show $ phaseS subs)
-  htmlContent <- T.fromStrict <$> TIO.readFile filename
-  sendMimeMail (addressS subs) "peter.ferenc.hajdu@gmail.com" "minink daily mail" "" htmlContent [] smtpConn
-  executeNamed conn "UPDATE subscription SET phase = :phase, lastsent = :lastsent WHERE address = :address" [":phase" := (phaseS subs) + 1, ":lastsent" := today, ":address" := (addressS subs)]
+import System.Directory (getAppUserDataDirectory, createDirectoryIfMissing, doesFileExist)
+import System.IO (FilePath, hPutStrLn, stderr)
+import System.Exit (exitFailure, exitSuccess)
 
-haveNotSentToday :: (Integer, Int, Int) -> Subscription -> Bool
-haveNotSentToday today (Subscription _ lastSent _) = let date = toGregorian' $ fromSeconds lastSent
-                                                     in date /= today
+data Config = Config
+  { smtpConnection :: SMTPConnection
+  , subsDbConnection :: SQL.Connection
+  , lessonBase :: String
+  }
+
+newtype Sender a = Sender {run :: ReaderT Config IO a} deriving (Functor, Applicative, Monad, MonadReader Config, MonadIO)
+
+instance EmailSender Sender where
+  sendEmail address content = do
+    connection <- asks smtpConnection
+    safeIO $ sendMimeMail address "peter.ferenc.hajdu@gmail.com" "minink daily mail" "" content [] connection
+
+instance SubscriptionDb Sender where
+  loadSubscriptions = do
+    dbConn <- asks subsDbConnection
+    safeIO $ SQL.query_ dbConn "SELECT * from subscription"
+
+  updateSubscription subs = do
+    dbConn <- asks subsDbConnection
+    let query = "UPDATE subscription SET phase = :phase, lastsent = :lastsent WHERE address = :address"
+    let params = [":phase" := (phaseS subs) + 1, ":lastsent" := (lastSentS subs), ":address" := (addressS subs)]
+    safeIO $ SQL.executeNamed dbConn query params
 
 
-runSender :: String -> IO ()
-runSender mininkBase = do
-  smtpConn <- connectSMTPSTARTTLSWithSettings "smtp.gmail.com" (Settings 587 0 True True)
-  result <- authenticate LOGIN "peter.ferenc.hajdu@gmail.com" "tcepjzibgwoqcwwy" smtpConn
+instance Epoch Sender where
+  currentTimeInEpoch = do
+    date <- liftIO getCurrentTime
+    return $ toSeconds date
 
-  conn <- open $ mininkBase ++ "subscriptions.db"
-  subscriptions <- query_ conn "SELECT * from subscription" :: IO [Subscription]
-  today <- getCurrentTime
-  let gregorian = toGregorian' today
-  let shouldSend = filter (haveNotSentToday gregorian) subscriptions
-  mapM_ (sendEmail mininkBase smtpConn (toSeconds today) conn) shouldSend
-  close conn
-  closeSMTP smtpConn
 
+instance LessonDb Sender where
+  retrieveLesson (Subscription phase _ _) = do
+    lessonBaseFolder <- asks lessonBase
+    let fileName = lessonBaseFolder ++ (show phase)
+    doesExist <- liftIO $ doesFileExist fileName
+    if doesExist
+      then do
+        maybeContent <- safeIO $ TIO.readFile fileName
+        return $ (Lesson . LT.fromStrict) <$> maybeContent
+      else return $ Right $ Finished
+
+runSender :: Config -> IO (Either [String] ())
+runSender config = runReaderT (run sendDailyMails) config
+
+createAppFolders :: IO (FilePath, FilePath)
+createAppFolders = do
+  mininkBase <- getAppUserDataDirectory "minink"
+  let lessonPath = mininkBase ++ "/lessons/"
+  let dbPath = mininkBase ++ "/subscriptions.db"
+  createDirectoryIfMissing True lessonPath
+  return (lessonPath, dbPath)
+
+printError :: String -> IO ()
+printError = hPutStrLn stderr
 
 main :: IO ()
 main = do
-  maybeHome <- lookupEnv "HOME"
-  let mininkBase = (++ "/.minink/") <$> maybeHome
-  maybe (pure ()) runSender mininkBase
+  (lessonPath, dbPath) <- createAppFolders
+  smtpConn <- connectSMTPSTARTTLSWithSettings "smtp.gmail.com" (Settings 587 0 True True)
+  authenticate LOGIN "peter.ferenc.hajdu@gmail.com" "tcepjzibgwoqcwwy" smtpConn
+  dbConn <- SQL.open dbPath
+  let config = Config smtpConn dbConn lessonPath
+  result <- runSender config
+  SQL.close dbConn
+  closeSMTP smtpConn
+  case result of
+    Left errors -> do
+      mapM_ printError errors
+      exitFailure
+    Right _ -> exitSuccess
+
+safeIO :: MonadIO m => IO a -> m (Either String a)
+safeIO action = liftIO $ handle catchAll $ Right <$> action
+
+catchAll :: SomeException -> IO (Either String a)
+catchAll = return . Left . show
