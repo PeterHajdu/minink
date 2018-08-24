2ebaa949da80441a32008dd82c690d78ffa24f4d
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2018-08-24 17:51:39 +0200

    switch to html and form instead of json

--- src-web/Main.hs
+++ src-web/Main.hs
@@ -7,29 +7,43 @@ module Main where
 
 import GHC.Generics
 import Servant
-import Data.Aeson.Types
+import Servant.HTML.Blaze
 import Network.Wai.Handler.Warp
 import Control.Monad.IO.Class (liftIO)
 import qualified Database.SQLite.Simple as SQL
+import Web.FormUrlEncoded (FromForm)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
 data SubscriptionRequest = SubscriptionRequest
-  { address :: String
+  { address :: !String
   } deriving (Generic, Show)
 
-instance FromJSON SubscriptionRequest
+instance FromForm SubscriptionRequest
 
-type SubscriptionApi = "subscription" :> ReqBody '[JSON] SubscriptionRequest :> Post '[JSON] NoContent
+type SubscriptionApi =
+  "subscription" :> ReqBody '[FormUrlEncoded] SubscriptionRequest :> Post '[HTML] NoContent :<|>
+  "subscription" :> Get '[HTML] H.Html
 
 dbFile :: FilePath
 dbFile = "/home/hptr/.minink/subscriptions.db"
 
 subscriptionServer :: Server SubscriptionApi
-subscriptionServer = subscribe
-  where subscribe :: SubscriptionRequest -> Handler NoContent
-        subscribe request = do
+subscriptionServer = post :<|> get
+  where post :: SubscriptionRequest -> Handler NoContent
+        post request = do
           liftIO $ SQL.withConnection dbFile $ \conn -> do
             SQL.execute conn "INSERT INTO requests VALUES (?)" $ SQL.Only (address request)
           return NoContent
+        get :: Handler H.Html
+        get = return form
+        form :: H.Html
+        form = H.docTypeHtml $ do
+          H.head $ H.title "somethingsomething"
+          H.body $ H.form H.! A.method "post" H.! A.action "/subscription" $ do
+            H.input H.! A.type_ "text" H.! A.name "address" H.! A.placeholder "email address"
+            H.input H.! A.type_ "submit" H.! A.value "Send"
+
 
 subscriptionApi :: Proxy SubscriptionApi
 subscriptionApi = Proxy
