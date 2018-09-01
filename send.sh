efd989b9e5800d65140b1eaf526476f1ded738e8
Peter Hajdu <peter.ferenc.hajdu@gmail.com>
2018-09-01 16:36:00 +0200

    Remove send script

--- send.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-curl --header "Content-Type: application/json" \
-     --request POST \
-     --data '{"address":"harcsa.bajusz@gmail.com"}' \
-     http://localhost:8081/subscription
